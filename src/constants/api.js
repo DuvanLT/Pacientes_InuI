52138f4dc0aa442f18925b212ab83a73979064f7
DuvanLT <duvanred0@gmail.com>
2025-02-10 13:25:50 -0500

    prueba para desplegar servicio backend en render

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const DB_FILE = 'data/data.json'
+export const DB_FILE = '/opt/render/project/src/src/data/data.json'
 export const PORT = 3000
 export const API_URL = `http://localhost:${PORT}`
